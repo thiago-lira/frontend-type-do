9bb0ca2cd3a4a837e1fbc03080ed1e47c8bf743f
Thiago Lira <lira_thiago@msn.com>
2021-07-25 22:06:22 -0300

    Adjust API endpoints

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -3,24 +3,24 @@ import Task from '../models/Task'
 
 class TaskService {
   static delete(id: string) {
-    return axios.delete(`/api/_doc/${id}/?refresh=wait_for`)
+    return axios.delete(`/api/${id}/?refresh=wait_for`)
   }
 
   static save(task: Task) {
-    return axios.post('/api/_doc/?refresh=wait_for', { task })
+    return axios.post('/api/?refresh=wait_for', task)
   }
 
   static update(id: number) {
-    return axios.put(`/api/_doc/${id}`)
+    return axios.put(`/api/${id}`)
   }
 
-  static getList() {
-    return axios.get('/api/_search?from=0')
+  static getList({ from = 0 } = {}) {
+    return axios.get(`/api/_search?from=${from}`)
       .then(({ data: { hits: { hits, total } } }) => {
         return {
           total: total.value,
           tasks: hits.map((item: any) => {
-            const { _id,  _source: { task: { _date, done, title } } } = item
+            const { _id,  _source: { _date, done, title } } = item
             return new Task(title, _date, done, _id)
           })
         }
